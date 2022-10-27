089aba923b7ce2966de0705465a23805fccaf1ca
Carolinaviviani1998 <carolinaviviani11@gmail.com>
2022-10-27 15:04:18 -0400

    updated db URI to work with MongoDb Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1/infor",
+    //"URI": "mongodb://127.0.0.1/infor",
+    "URI":  "mongodb+srv://carolina:kbOvOgbUfc66sqi7@mongodbserver.afzqgfh.mongodb.net/infor?retryWrites=true&w=majority",
    
     "Secret": 'SomeSecret'
 }
